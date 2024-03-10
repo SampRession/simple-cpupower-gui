c5655f90dc81052326eca8de810f32f0ae7858b0
Bew <bastsamp@outlook.com>
2024-03-10 01:24:23 +0100

    Layout + features (buggy) / before switch to IntVar

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ from tkinter import ttk
 
 class MainWindow:
     def __init__(self, root):
+        self.root = root
         root.title("CPU clock speed limiter")
-        root.geometry("650x400")
-        root.resizable(False, False)
+        root.geometry("765x250")
+        # root.resizable(False, False)
         
         main_frame = ttk.Frame(root)
-        main_frame.pack()
         
         # Variables
         self.min_clock_speed = tk.DoubleVar(value=0.8)
@@ -24,53 +24,84 @@ class MainWindow:
         self.governor_policy = tk.StringVar(value="powersave")
         
         # Governor policy
-        gov_frame = ttk.Frame(main_frame, padding=(5, 10))
-        gov_label = ttk.Label(gov_frame, text="Set governor policy")
-        gov_label.pack()
+        gov_frame = ttk.Frame(main_frame, padding=(5, 15, 100, 5))
+        gov_label = ttk.Label(gov_frame, text="Set governor policy",
+                              padding=(0, 0, 0, 5))
+        gov_label["font"] = "Helvetica 14"
         powersave_policy = ttk.Radiobutton(gov_frame,
                                            text="Powersave",
                                            variable=self.governor_policy,
                                            value="powersave",
                                            command=self.set_governor)
-        powersave_policy.pack()
+        # powersave_policy.configure(style=)
         performance_policy = ttk.Radiobutton(gov_frame,
                                            text="Performance",
                                            variable=self.governor_policy,
                                            value="performance",
                                            command=self.set_governor)
-        performance_policy.pack()
-        gov_frame.pack()
+        gov_label.grid(column=0, row=0, sticky="W")
+        powersave_policy.grid(column=0, row=1)
+        performance_policy.grid(column=0, row=2)
+        gov_frame.grid(column=0, row=3)
         
         
         # Sliders
-        sliders_frame = ttk.Frame(main_frame, padding=(5, 10))
+        sliders_frame = ttk.Frame(main_frame, padding=5)
         sliders_label = ttk.Label(sliders_frame, 
-                                  text="Set minimum and maximum clock speed")
-        sliders_label.pack()
-        min_speed_scale = ttk.Scale(master=sliders_frame,
-                                    length=200,
+                                  text="Set clock speed scale",
+                                  padding=(0, 0, 0, 5))
+        sliders_label["font"] = "Helvetica 14"
+        
+        min_speed_label = ttk.Label(sliders_frame,
+                                    text="Min speed")
+        min_speed_slider = ttk.Scale(master=sliders_frame,
+                                    length=175,
                                     from_=0.8,
                                     to=4.5,
-                                    variable=self.min_clock_speed)
-        min_speed_scale.pack()
-        max_speed_scale = ttk.Scale(master=sliders_frame,
-                                    length=200,
+                                    variable=self.min_clock_speed,
+                                    command=self.set_min_speed)
+        min_speed_value = ttk.Label(sliders_frame, 
+                                    # text=f"{self.set_min_speed('value')}",
+                                    textvariable=self.min_clock_speed)
+        
+        max_speed_label = ttk.Label(sliders_frame,
+                                    text="Max speed")
+        max_speed_slider = ttk.Scale(master=sliders_frame,
+                                    length=175,
                                     from_=0.8,
                                     to=4.5,
-                                    variable=self.max_clock_speed)
-        sliders_frame.grid_columnconfigure()
-        min_speed_scale.pack()
-        max_speed_scale.pack()
-        sliders_frame.pack()
+                                    variable=self.max_clock_speed,
+                                    command=self.set_max_speed)
+        max_speed_value = ttk.Label(sliders_frame, 
+                                    # text=f"{self.set_min_speed('value')}",
+                                    textvariable=self.max_clock_speed)
+        
+        sliders_label.grid(column=0, row=0, columnspan=3, sticky="w")
+        min_speed_label.grid(column=0, row=1, padx=5)
+        min_speed_slider.grid(column=1, row=1, pady=5)
+        min_speed_value.grid(column=2, row=1, pady=5)
+        max_speed_label.grid(column=0, row=2, padx=5)
+        max_speed_slider.grid(column=1, row=2, pady=5)
+        max_speed_value.grid(column=2, row=2, pady=5)
         
         # Confirm button
-        confirm_frame = ttk.Frame(main_frame, padding=(5, 10))
+        confirm_frame = ttk.Frame(main_frame)
         confirm_button = ttk.Button(master=confirm_frame, 
                                     text="Confirm",
                                     command=self.validate_settings)
         confirm_button.pack()
-        confirm_frame.pack()
-
+        
+        # Output Label Frame
+        output_frame = ttk.Labelframe(main_frame, text="Terminal Output",
+                                      width=400, height=200)
+        
+        # Main Grid layout
+        main_frame.grid(column=0, row=0)
+        gov_frame.grid(column=0, row=0)
+        sliders_frame.grid(column=0, row=1)
+        output_frame.grid(column=1, row=0, rowspan=2,padx=15, sticky="E")
+        confirm_frame.grid(column=0, row=2, columnspan=2, pady=15)
+        
     def set_governor(self):
         governor_policy = self.governor_policy.get()
         return governor_policy
@@ -83,6 +114,7 @@ class MainWindow:
     def set_max_speed(self):
         max_speed = self.max_clock_speed.get()
         max_speed = round(max_speed, 1)
+        print(max_speed)
         return max_speed
         
     # Provisoire
@@ -93,6 +125,10 @@ class MainWindow:
         print(f"Current speed scale is: {min_speed}<-->{max_speed}\n"
               f"Current governor policy is: {governor_policy}")
     
+    def round_min_value(self):
+        min_value = self.min_clock_speed.get()
+        round_min_value = round(min_value, 1)
+        return round_min_value
 
 
 def main():
